d526ff487e61ffc2861089da608b86592cf7eece
seeaya <connorbarnesdenver@icloud.com>
2021-01-14 18:00:47 -0700

    Version 1.0.0

---
+++ Headers/CVISATypes-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  CVISATypes-Bridging-Header.h
+//  
+//
+//  Created by Connor Barnes on 12/2/20.
+//
+
+#ifndef CVISATypes_Bridging_Header_h
+#define CVISATypes_Bridging_Header_h
+
+#import "/Library/Frameworks/VISA.framework/Headers/visatype.h"
+
+#endif /* CVISATypes_Bridging_Header_h */
