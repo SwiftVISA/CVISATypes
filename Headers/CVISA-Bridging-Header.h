14d400f471c4f3c62a4add40ab7b85152080512a
seeaya <connorbarnesdenver@icloud.com>
2021-01-14 17:19:31 -0700

    Changed the library to expose only CVISA types instead of functions.

--- Headers/CVISA-Bridging-Header.h
+++ Headers/CVISA-Bridging-Header.h
@@ -1,22 +1,13 @@
 //
-//  Header.h
+//  CVISA-Bridging-Header.h
 //  
 //
 //  Created by Connor Barnes on 12/2/20.
 //
 
-#ifndef Header_h
-#define Header_h
+#ifndef CVISA_Bridging_Header_h
+#define CVISA_Bridging_Header_h
 
-//#define MY_FLAG
-//
-//#if __has_include("/Library/Frameworks/VISA.framework/Headers/visa.h") && __has_include("/Library/Frameworks/VISA.framework/Headers/visatype.h")
-
-#define HAS_NI_VISA 1
-
-#import "/Library/Frameworks/VISA.framework/Headers/visa.h"
 #import "/Library/Frameworks/VISA.framework/Headers/visatype.h"
 
-//#endif
-
-#endif /* Header_h */
+#endif /* CVISA_Bridging_Header_h */
